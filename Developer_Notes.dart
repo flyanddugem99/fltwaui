89dfa40734eac32007cb9b50fd05072d2fe55b56
flyanddugem99 <111583992+flyanddugem99@users.noreply.github.com>
2023-02-27 17:28:42 +0700

    Create Developer_Notes.dart

---
+++ Developer_Notes.dart
@@ -0,0 +1,11 @@
+//Buat ChatSample.dart, Notes :
+//
+//ada overflow
+//kalo mau buat circle di container pake shape aja, jangan maksa borderRadius
+//pakai shape: BoxShape.circle
+//gambar user pake CircleAvatar saja
+//try to explore banyak widget widget lain.
+//try to make percobaan user interface buat aplikasi aplikasi lain, kayak contoh facebook, atau line, atau juga bisa kayak website website yang ada di internet
+//kayak netflix, youtube, atau twitter
+//
+// try next time, goodgame.
